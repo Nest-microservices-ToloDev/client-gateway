ca5e1a7f971070122ea2d1319d4b0b4d8588dd4e
Tolomatico <ballesty.t@gmail.com>
2025-05-21 02:00:04 -0300

    add esception errors

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ app.useGlobalPipes(
   app.useGlobalFilters(new RpcCustomExceptionFilter())
 
   await app.listen(envs.port);
+
   logger.log(`Server listening on port:${envs.port}`)
 }
 bootstrap()
